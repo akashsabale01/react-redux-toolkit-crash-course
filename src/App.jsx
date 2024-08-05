1cf1ca5a83f515c85ace299524b9bacb13c67ba6
akashsabale01 <akashsabale100@gmail.com>
2024-08-05 21:44:50 +0530

    added initial code state without redux-toolkit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Products.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    console.log("data -> ", data);
+    setProducts(data);
+  };
+
+  return (
+    <div className="productsWrapper">
+      {
+      products.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.image} alt="image not found" />
+          <h4>{product.title}</h4>
+          <h5>{product.price}</h5>
+          <button className="btn">Add to Cart</button>
+        </div>
+      ))
+      }
+    </div>
+  );
+};
+
+export default Products;
